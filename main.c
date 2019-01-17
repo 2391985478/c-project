648f606855fd69d9add07c190cec9098111f8a34
杨自超 <2391985478@qq.com>
2019-01-17 08:52:33 +0800

    second

--- main.c
+++ main.c
@@ -2,7 +2,45 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("jinrumingnacixitong\n");
+
+    while(1){
+
+        printf("1---tianjia\n");
+        printf("2---shanchu\n");
+        printf("3---xianshi\n");
+        printf("4---tihuan\n");
+        printf("5---tuichu\n");
+
+        printf("xuanze gongneng\n");
+
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+
+        }
+        if(code == 2){
+
+            
+        }
+        if(code == 3){
+
+            
+        }
+        if(code == 4){
+
+            
+        }
+        if(code == 5){
+            printf("tuichuxitong\n");
+            break;
+            
+        }
+    }
+    
+
 
     return 0;
 
